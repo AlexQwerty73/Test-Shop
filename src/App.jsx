dd14f7abee69fa392cc74c305e32674956dec0d9
Oleksii Tuhai <alextugai73@gmail.com>
2024-01-28 18:14:08 +0100

    create Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 const App = () => {
   return (
     <div className="App">
-      
-      
+
+      <Routes>
+        <Route path='/'>
+
+        </Route>
+      </Routes>
 
     </div>
   );
