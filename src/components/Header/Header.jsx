be1b72312a9c80a94e27d57a0ee0045112107ae9
Oleksii Tuhai <alextugai73@gmail.com>
2024-01-29 15:40:02 +0100

    add logo component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Logo } from '../common/Logo/Logo';
 
 export const Header = () => {
    return (
       <header className='header'>
          <div className="container">
-            
+
+            <Logo />
+
          </div>
       </header>
    );
