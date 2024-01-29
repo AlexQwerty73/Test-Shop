68433f99b8f1affb7a1baf8c2b678dad1983494f
Oleksii Tuhai <alextugai73@gmail.com>
2024-01-29 15:44:31 +0100

    add link to logo

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import s from './logo.module.css';
+import { Link } from 'react-router-dom';
 
 export const Logo = () => {
    return (
       <div className={s.logo}>
+         <Link to='/'>
          <img className={s.logo__img} src="/imgs/logo.png" alt="Logo" />
+         </Link>
       </div>
    );
 };
